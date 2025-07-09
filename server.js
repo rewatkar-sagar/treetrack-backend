e3b095cbbde49983a51fc0c8c1f2f70cdaa080b0
rewatkar-sagar <rewatkarsagar711@gmail.com>
2025-07-09 10:43:15 +0530

    Backend ready for Render

--- server.js
+++ server.js
@@ -76,8 +76,7 @@ app.get('/trees', async (req, res) => {
   res.json(trees);
 });
 // Start server
-sequelize.sync().then(() => {
-  app.listen(4000, () => {
-    console.log('Server running...');
-  });
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
